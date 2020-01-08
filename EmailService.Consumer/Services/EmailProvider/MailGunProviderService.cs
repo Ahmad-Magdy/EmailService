c3e7fe083a178c1d69f5efbea2c52c49326f9a5f
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-08 23:58:39 +0100

    Mailgun implementation

---
+++ EmailService.Consumer/Services/EmailProvider/MailGunProviderService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Net;
+using EmailService.Consumer.Config;
+using EmailService.Consumer.Utils;
+
+namespace EmailService.Consumer.Services.EmailProvider
+{
+    public class MailGunProviderService
+    {
+        public class MailGunProvider : IEmailProvider
+        {
+            ILogger<MailGunProvider> _logger;
+            private HttpClient _httpClient;
+            private IOptionsMonitor<ConfigOptions> _emailProvidersConfig;
+
+            public MailGunProvider(IHttpClientFactory httpClientFactory, IOptionsMonitor<ConfigOptions> emailProvidersConfig, ILogger<MailGunProvider> logger)
+            {
+                _emailProvidersConfig = emailProvidersConfig;
+                _httpClient = httpClientFactory.CreateClient();
+                _httpClient.BaseAddress = new Uri(emailProvidersConfig.CurrentValue.EmailProviders.MailGun.BaseUrl);
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                                                    Authentication.GetBasicAuthentication("api", _emailProvidersConfig.CurrentValue.EmailProviders.MailGun.ApiKey));
+                _logger = logger;
+            }
+
+            public async Task SendEmail(string sender, string reciver, string subject, string body)
+            {
+                var domain = _emailProvidersConfig.CurrentValue.EmailProviders.MailGun.Domain;
+                var emailDetails = new Dictionary<string, string> {
+                {"from", $"{sender}" },
+                {"to", reciver},
+                {"subject", subject},
+                {"html",body }
+            };
+                var response = await _httpClient.PostAsync($"v3/{domain}/messages", new FormUrlEncodedContent(emailDetails));
+                _logger.LogInformation($"Sent Email with response {(int)response.StatusCode}");
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    _logger.LogError(responseBody);
+                }
+            }
+        }
+    }
+}
---
+++ EmailService.Consumer/Utils/Authentication.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Text;
+
+namespace EmailService.Consumer.Utils
+{
+    public static class Authentication
+    {
+        public static string GetBasicAuthentication(string username, string password) => Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+    }
+}
