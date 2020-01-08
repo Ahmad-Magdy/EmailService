bd98778bad75417c68ab384324475da699d6238b
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-08 23:49:47 +0100

    Design interface for the service

---
+++ EmailService.Consumer/Services/EmailProvider/IEmailProviderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EmailService.Consumer.Services.EmailProvider
+{
+    public class IEmailProviderService
+    {
+        public interface IEmailProvider
+        {
+            Task SendEmail(string sender, string reciver, string subject, string body);
+        }
+    }
+}
