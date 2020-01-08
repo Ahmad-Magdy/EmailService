9bba7eafe199b723ceac1320bf144aed3657f3c2
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-08 23:52:15 +0100

    Add sendgrid simple implementation

---
+++ EmailService.Consumer/Services/EmailProvider/SendGridProviderService.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net;
+
+namespace EmailService.Consumer.Services.EmailProvider
+{
+    public class SendGridProviderService : IEmailProvider
+    {
+        private SendGridClient _sendGridClient;
+        ILogger<SendGridProviderService> _logger;
+
+        public SendGridProviderService(SendGridClient sendGridClient, ILogger<SendGridProviderService> logger)
+        {
+            _sendGridClient = sendGridClient;
+            _logger = logger;
+        }
+
+        public async Task SendEmail(string sender, string reciver, string subject, string body)
+        {
+            var from = new EmailAddress(sender);
+            var to = new EmailAddress(reciver);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, "", body);
+
+            var response = await _sendGridClient.SendEmailAsync(msg);
+            _logger.LogInformation($"Sent Email with response {(int)response.StatusCode}");
+            if (response.StatusCode != HttpStatusCode.Accepted)
+            {
+                var responseBody = await response.Body.ReadAsStringAsync();
+                _logger.LogError(responseBody);
+            }
+
+        }
+    }
+}
