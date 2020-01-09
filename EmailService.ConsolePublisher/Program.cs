981178bb45b11468dc30a55cb034250c1d761383
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-09 22:28:47 +0100

    Add console client

---
+++ EmailService.ConsolePublisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace EmailService.ConsolePublisher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var emailConnetioString = Environment.GetEnvironmentVariable("EmailServiceStorageCS");
+            var queueName = Environment.GetEnvironmentVariable("QueueName");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(emailConnetioString);
+
+            var queueClient = storageAccount.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference(queueName);
+            await queue.CreateIfNotExistsAsync();
+            var count = 0;
+            while(count <= 5000)
+            {
+                var emailItem = new EmailQueueItem
+                {
+                    Sender = "Me@me.com",
+                    Reciver = "m2@m.com",
+                    Subject = "Subject 1",
+                    Body = "Test"
+                };
+                var emailQueueItem = new CloudQueueMessage(JsonConvert.SerializeObject(emailItem));
+                await queue.AddMessageAsync(emailQueueItem);
+                Console.WriteLine("Message Sent");
+                count++;
+            }
+        }
+    }
+
+    public class EmailQueueItem
+    {
+        public string Sender { get; set; }
+        public string Reciver { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
