4851d06cf294f4962ec0326ea54f4410b64329d4
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-09 20:25:03 +0100

    Add queue item class type

---
+++ EmailService.Consumer/Models/EmailQueueItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmailService.Consumer.Models
+{
+    public class EmailQueueItem
+    {
+        public string Sender { get; set; }
+        public string Reciver { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
