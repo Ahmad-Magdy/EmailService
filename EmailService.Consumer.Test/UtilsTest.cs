65ee1a47636508f40572179f3a60f598735cbb55
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-10 00:12:51 +0100

    Add test for utils

---
+++ EmailService.Consumer.Test/UtilsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EmailService.Consumer.Utils;
+using Xunit;
+
+namespace EmailService.Consumer.Test
+{
+    public class UtilsTest
+    {
+        [Fact]
+        public void BasicAuthenticationBase64()
+        {
+            string expected = "dXNlcm5hbWU6cGFzc3dvcmQ=";
+            string actual = Authentication.GetBasicAuthentication("username", "password");
+
+            Assert.StrictEqual(expected, actual);
+        }
+    }
+}
