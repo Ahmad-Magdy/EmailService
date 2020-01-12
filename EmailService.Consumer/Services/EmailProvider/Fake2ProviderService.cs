151ccb42db71df2413ba64e01f9ec41384fb2d4c
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-12 12:41:37 +0100

    Add fake2 provider

---
+++ EmailService.Consumer/Services/EmailProvider/Fake2ProviderService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace EmailService.Consumer.Services.EmailProvider
+{
+    public class Fake2ProviderService : IEmailProvider
+    {
+        private readonly ILogger<FakeProviderService> _logger;
+
+        public Fake2ProviderService(ILogger<FakeProviderService> logger)
+        {
+            _logger = logger;
+        }
+
+        public string ProviderName => "Fake2";
+
+        public async Task SendEmail(string sender, string reciver, string subject, string body)
+        {
+            // Do Some Magic
+            _logger.LogInformation("Calling SendEmail from Fake email service2 ");
+            await Task.Delay(TimeSpan.FromMilliseconds(200));
+        }
+    }
+}
