58d1e6e6d738698ea5a639f194927a61dc2cd519
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-12 20:31:52 +0100

    Add Helper method to read array of env var

---
+++ EmailService.Consumer/Utils/EnvironmentVariables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmailService.Consumer.Utils
+{
+    public static class EnvironmentVariables
+    {
+        public static List<string> EnvironmentVariableKeyToArray(string key)
+        {
+            var list = new List<string>();
+            int index = 0;
+            while (true)
+            {
+                var val = Environment.GetEnvironmentVariable($"{key}__{index}");
+                if (string.IsNullOrEmpty(val))
+                    break;
+                list.Add(val);
+                index++;
+            }
+            return list;
+        }
+    }
+}
