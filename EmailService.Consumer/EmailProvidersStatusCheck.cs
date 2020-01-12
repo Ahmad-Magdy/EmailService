95d64e232117931915f7dd4a16e77c1f1bbb9d0d
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-12 12:43:22 +0100

    Add function to check the providers status

---
+++ EmailService.Consumer/EmailProvidersStatusCheck.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace EmailService.Consumer
+{
+    public static class EmailProvidersStatusCheck
+    {
+        [FunctionName("EmailProvidersStatusCheck")]
+        public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer, [DurableClient] IDurableEntityClient client, ILogger log)
+        {
+            var entityId = new EntityId(nameof(EmailProvidersStatus), "emailProviderState");
+            await client.SignalEntityAsync<IEmailProvidersStatus>(entityId, s => s.CheckDisabledProvidersStatus());
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
