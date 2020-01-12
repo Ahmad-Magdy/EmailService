3b4de1ad34ccae5d36b4108f6cff8167d6f9e4a0
Ahmed Magdy <ahmedmagdi@outlook.com>
2020-01-12 21:35:41 +0100

    Add IDurableEntityClient to the test

--- EmailService.Consumer.Test/EmailTestService.cs
+++ EmailService.Consumer.Test/EmailTestService.cs
@@ -10,6 +10,8 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Options;
 using EmailService.Consumer.Config;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using EmailService.Consumer.Models.Config;
 
 namespace EmailService.Consumer.Test
 {
@@ -24,12 +26,20 @@ namespace EmailService.Consumer.Test
             var emailProvider = TestFactory.CreateFakeEmailProvider(emailProviderlogger);
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddSingleton<IEmailProvider>(emailProvider);
-
-            var options = Options.Create(new ConfigOptions() { Sentry = new SentryOptions(), EmailProviders = new EmailProviders { SendGrid = new Config.SendGrid { ApiKey = "" } } });
+            var durableClient = new Mock<IDurableEntityClient>();
+            durableClient.Setup(x => x.ReadEntityStateAsync<EmailProvidersStatus>(It.IsAny<EntityId>(),null, null))
+                .Returns(Task.FromResult(new EntityStateResponse<EmailProvidersStatus>
+                {
+                    EntityExists = false,
+                    EntityState = null
+                }));
+            var options = Options.Create(new ConfigOptions() { Sentry = new SentryOptions(),
+                                EmailProviders = new EmailProviders { SendGrid = new Config.SendGrid { ApiKey = "" } },
+                    EmailProvidersSettings= new EmailProvidersSettings {SupportedProviders=new List<string> { "Fake" } } });
 
             var emailServiceFunction = new EmailService(emailProvider, functionLogger, serviceCollection.BuildServiceProvider(), options);
 
-            await emailServiceFunction.RunAsync(new Models.EmailQueueItem { Sender = "me@test.dk", Reciver = "reciver@test.dk", Subject = "Subject", Body = "MyText" });
+            await emailServiceFunction.RunAsync(new Models.EmailQueueItem { Sender = "me@test.dk", Reciver = "reciver@test.dk", Subject = "Subject", Body = "MyText" }, durableClient.Object);
             var msg = functionLogger.Logs[0];
             Assert.Contains("The request processing time was", msg);
         }
@@ -40,14 +50,28 @@ namespace EmailService.Consumer.Test
             var emailProviderlogger = (ListLogger<FakeProviderService>)TestFactory.CreateLogger<FakeProviderService>(LoggerTypes.List);
             var functionLogger = (ListLogger<EmailService>)TestFactory.CreateLogger<EmailService>(LoggerTypes.List);
             var emailProvider = TestFactory.CreateFakeEmailProvider(emailProviderlogger);
+
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddSingleton<IEmailProvider>(emailProvider);
 
-            var options = Options.Create(new ConfigOptions() { Sentry = new SentryOptions(), EmailProviders = new EmailProviders { SendGrid = new Config.SendGrid { ApiKey = "" } } });
+            var durableClient = new Mock<IDurableEntityClient>();
+            durableClient.Setup(x => x.ReadEntityStateAsync<EmailProvidersStatus>(It.IsAny<EntityId>(), null, null))
+                .Returns(Task.FromResult(new EntityStateResponse<EmailProvidersStatus>
+                {
+                    EntityExists = false,
+                    EntityState = null
+                }));
+
+            var options = Options.Create(new ConfigOptions()
+            {
+                Sentry = new SentryOptions(),
+                EmailProviders = new EmailProviders { SendGrid = new Config.SendGrid { ApiKey = "" } },
+                EmailProvidersSettings = new EmailProvidersSettings { SupportedProviders = new List<string> { "Fake" } }
+            });
 
             var emailServiceFunction = new EmailService(emailProvider, functionLogger, serviceCollection.BuildServiceProvider(), options);
 
-            await emailServiceFunction.RunAsync(new Models.EmailQueueItem { });
+            await emailServiceFunction.RunAsync(new Models.EmailQueueItem { }, durableClient.Object);
 
             var msg = functionLogger.Logs[0];
             Assert.Contains("'Sender' must not be empty", msg);
